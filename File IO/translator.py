1df3bfc009907a62141749cc7fe9c90213d088ba
Youping Nicole Li <97087333+ChemistL@users.noreply.github.com>
2022-02-08 11:33:40 -0800

    Update translator.py

--- File IO/translator.py
+++ File IO/translator.py
@@ -6,10 +6,11 @@ Created on Sun Aug 23 13:49:36 2020
 """
 from translate import Translator
 
+translator= Translator(to_lang="ja")
+try: 
 with open('translate.txt', mode = 'r') as my_file:
     text = my_file.read()
-
-translator= Translator(to_lang="es")
-translation = translator.translate(text)
+    translation = translator.translate(text)
+    with open('./test-ja.txt', mode = 'w') as my_file2
 
 print(translation)
