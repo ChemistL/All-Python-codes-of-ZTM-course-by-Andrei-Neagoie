f9d0d42898d258e9a9470b50dee827e2d7f2df7b
Youping Nicole Li <97087333+ChemistL@users.noreply.github.com>
2022-02-08 11:39:32 -0800

    Update translator.py

--- File IO/translator.py
+++ File IO/translator.py
@@ -8,9 +8,13 @@ from translate import Translator
 
 translator= Translator(to_lang="ja")
 try: 
-with open('translate.txt', mode = 'r') as my_file:
-    text = my_file.read()
-    translation = translator.translate(text)
-    with open('./test-ja.txt', mode = 'w') as my_file2
+    with open('translate.txt', mode = 'r') as my_file:
+        text = my_file.read()
+        translation = translator.translate(text)
+        with open('./test-ja.txt', mode = 'w') as my_file2    #created a test-ja txt. on your desktop
+            my_file2.write(translation)    #created a translted file in japanese 
 
-print(translation)
+except FileNotFoundERROR as e:
+    print('check your file path silly?')
+    
+    
