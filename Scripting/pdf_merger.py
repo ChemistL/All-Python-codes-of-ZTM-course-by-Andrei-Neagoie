d63aff86a405f58488f95d797fbcbb80e300541f
Youping Nicole Li <97087333+ChemistL@users.noreply.github.com>
2022-02-23 13:01:25 -0800

    Update pdf_merger.py

--- Scripting/pdf_merger.py
+++ Scripting/pdf_merger.py
@@ -10,9 +10,9 @@ import sys
 inputs = sys.argv[1:]   # this will stores all the arguments passes except the first one, and store them in a list
 
 def pdf_merger(pdf_list):
-    merger = PyPDF2.PdfFileMerger()
+    merger = PyPDF2.PdfFileMerger() # merge object is created
     for pdf in pdf_list:
-        merger.append(pdf)
+        merger.append(pdf) #append all pdf on the list in this loop until the last one is done
     merger.write('./PDF/merged.pdf')
 
 pdf_merger(inputs)
