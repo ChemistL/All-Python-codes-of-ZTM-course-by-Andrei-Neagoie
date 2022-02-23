783be8fc05aaa8065cd11d7f9f389c40c792bdef
Youping Nicole Li <97087333+ChemistL@users.noreply.github.com>
2022-02-23 11:08:53 -0800

    Create jpg_to_png.py

--- Scripting/jpg_to_png.py
+++ Scripting/jpg_to_png.py
@@ -8,8 +8,8 @@ new_path = sys.argv[2]
 if not os.path.exists(new_path):
     os.makedirs(new_path)
 
-for file in os.listdir(img_path):
-    img = Image.open(f'{img_path}{file}')
+for file in os.listdir(img_path):   # this step gives the image folder 
+    img = Image.open(f'{img_path}{file}') # this image is located in a different path and filename
     # new_file = file.replace(".jpg", ".png")    # we can do this or the below command
     new_file = os.path.splitext(file)[0]    # this keeps the file name without the extension
     img.save(f"{new_path}/{new_file}.png", "png")
