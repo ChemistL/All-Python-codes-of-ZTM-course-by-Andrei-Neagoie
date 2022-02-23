98dfb81abe49249dc01b782e2863efafa9afe980
Youping Nicole Li <97087333+ChemistL@users.noreply.github.com>
2022-02-23 11:20:58 -0800

    Update pdf.py

--- Scripting/pdf.py
+++ Scripting/pdf.py
@@ -4,9 +4,9 @@ Created on Tue Aug 25 10:08:18 2020
 
 @author: saura
 """
-import PyPDF2
+import PyPDF2 # the package that installs the PDF functions 
 
-with open('./PDF/dummy.pdf', 'rb') as file:      
+with open('./PDF/dummy.pdf', 'rb') as file:       
 # 'rb' is read binary, for pdf we append 'b' to it.
 # so it converts the file to binary and PyPDF2 works with binary files.
     reader = PyPDF2.PdfFileReader(file)
@@ -15,11 +15,11 @@ with open('./PDF/dummy.pdf', 'rb') as file:
     print(reader.numPages)
     print(reader.getPage(0))
 
-    page = reader.getPage(0)
+    page = reader.getPage(0) # page object has to be assigned before rotating, otherwise python dosn't recognize reader. rotateClockwise
     page.rotateClockwise(90)
     writer = PyPDF2.PdfFileWriter()
     writer.addPage(page)
 
-    with open('./PDF/rotated.pdf', 'wb') as new_file:
+    with open('./PDF/rotated.pdf', 'wb') as new_file:  #'wb' stands for write the file in binary format 
         writer.write(new_file)
 
